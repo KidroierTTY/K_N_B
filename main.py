c55cca6594ef7d98c07f24088cc05b904cd28bba
KdroierTTY <kidroiertty@gmail.com>
2024-11-06 16:31:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,46 +12,46 @@ while True:
 
     print("Я выбрал а ты?")
 
-    visor = input("Выбирай из (К/Н/Б): ")
+    vibor = input("Выбирай из (К/Н/Б): ")
 
-    visor = visor.lower()
+    vibor = vibor.lower()
 
     ti.sleep(1)
 
     print("Молодец просчитываю кто выиграл)")
 
-    if visor == "к" or visor == "н" or visor == "б":
+    if vibor == "к" or vibor == "н" or vibor == "б":
         if n == 1:
-            if visor == str("к"):
+            if vibor == str("к"):
                 print("Я выбрал тоже самое что и ты (К/К)")
 
-            elif visor == str("н"):
+            elif vibor == str("н"):
                 print("Я выиграл! (К/Н)")
 
-            elif visor == str("б"):
+            elif vibor == str("б"):
                 print("Вы выиграли! (К/Б)")
 
         elif n == 2:
-            if visor == str("к"):
+            if vibor == str("к"):
                 print("Вы выиграли! (Н/К)")
 
-            elif visor == str("н"):
+            elif vibor == str("н"):
                 print("Я выбрал тоже самое что и ты (Н/Н)")
 
-            elif visor == str("б"):
+            elif vibor == str("б"):
                 print("Я выиграл! (Н/Б)")
 
         elif n == 3:
-            if visor == str("к"):
+            if vibor == str("к"):
                 print("Я выиграл! (Б/К)")
 
-            elif visor == str("н"):
+            elif vibor == str("н"):
                 print("Вы выиграли! (Б/Н)")
 
-            elif visor == str("б"):
+            elif vibor == str("б"):
                 print("Я выбрал тоже самое что и ты (Б/Б)")
 
-    elif visor != "к" or visor != "н" or visor != "б":
+    elif vibor != "к" or vibor != "н" or vibor != "б":
         print("Вы выбрали не правильный символ!!!")
 
     ti.sleep(0.5)
